b29910dd91aa39c7b0cdc88729b561c182d2f504
ValentinCerneanu <valentincerneanu28@gmail.com>
2020-01-02 13:23:45 +0200

    finishing inserting

--- equipment_rental_business.sql
+++ equipment_rental_business.sql
@@ -2,8 +2,8 @@
 -- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 02, 2020 at 11:38 AM
+-- Host: 127.0.0.1
+-- Generation Time: Jan 02, 2020 at 12:21 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -15,14 +15,13 @@ SET time_zone = "+00:00";
 CREATE DATABASE `Equipment_Rental_Business`;
 USE `Equipment_Rental_Business`;
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Equipment_Rental_Business`
+-- Database: `equipment_rental_business`
 --
 
 -- --------------------------------------------------------
@@ -35,8 +34,18 @@ CREATE TABLE `bills` (
   `id` int(11) NOT NULL,
   `bill_no` varchar(255) DEFAULT NULL,
   `value` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `order_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bills`
+--
+
+INSERT INTO `bills` (`id`, `bill_no`, `value`, `created_at`, `order_id`) VALUES
+(1, NULL, 10, '2020-01-02 10:59:30', 1),
+(2, NULL, 23, '2020-01-02 11:00:20', 2),
+(3, NULL, 230, '2020-01-02 11:01:12', 3);
 
 -- --------------------------------------------------------
 
@@ -51,6 +60,14 @@ CREATE TABLE `bookings` (
   `description` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `interval_start`, `interval_finish`, `description`) VALUES
+(1, '2019-12-31 00:00:00', '2020-01-01 00:00:00', 'New Years Party'),
+(2, '2020-01-06 00:00:00', '2020-01-06 00:00:00', 'Saint John');
+
 -- --------------------------------------------------------
 
 --
@@ -62,6 +79,17 @@ CREATE TABLE `categories` (
   `name` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES
+(1, 'TV SETS', NULL),
+(2, 'AUDIO', NULL),
+(3, 'FURNITURE', NULL),
+(4, 'LIGHTS', NULL),
+(5, 'VR SETS', NULL);
 
 -- --------------------------------------------------------
 
@@ -77,6 +105,15 @@ CREATE TABLE `clients` (
   `country_code` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `name`, `email`, `phone`, `country_code`, `created_at`) VALUES
+(1, 'Valentin Cerneanu', 'valentincerneanu@gmail.com', '0730', 1, '2020-01-01 20:50:08'),
+(2, 'Alexandru Mihai Manolescu', 'alexmanolescu@gmail.com', '0730', 1, '2020-01-01 20:50:08'),
+(3, 'Vasile Vioara', 'VasileVioara@yahoo.de', '4903', 2, '2020-01-01 20:51:22');
 
 -- --------------------------------------------------------
 
@@ -97,7 +134,9 @@ CREATE TABLE `countries` (
 INSERT INTO `countries` (`code`, `name`, `continent_name`) VALUES
 (1, 'ROMANIA', 'EUROPE'),
 (2, 'GERMANY', 'EUROPE'),
-(3, 'UNITED STATES OF AMERICA', 'NORTH AMERICA');
+(3, 'UNITED STATES OF AMERICA', 'NORTH AMERICA'),
+(4, 'FRANCE', 'EUROPE'),
+(5, 'LITHUANIA', 'EUROPA');
 
 -- --------------------------------------------------------
 
@@ -112,6 +151,15 @@ CREATE TABLE `delivery_types` (
   `price` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `delivery_types`
+--
+
+INSERT INTO `delivery_types` (`id`, `name`, `description`, `price`) VALUES
+(1, 'Pick up from warehouse', NULL, 0),
+(2, 'Pick up from sale office', NULL, 0),
+(3, 'Delivery at address', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -122,6 +170,17 @@ CREATE TABLE `departments` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `name`) VALUES
+(1, 'Sales'),
+(2, 'Logistics'),
+(3, 'Technicians'),
+(4, 'Human Resources'),
+(5, 'Accountancy');
 
 -- --------------------------------------------------------
 
@@ -136,6 +195,18 @@ CREATE TABLE `devices` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `devices`
+--
+
+INSERT INTO `devices` (`id`, `name`, `description`, `created_at`) VALUES
+(1, 'Samsung_G4', NULL, '2020-01-01 20:57:19'),
+(2, 'Samsung_G4', NULL, '2020-01-01 20:57:19'),
+(3, 'Samsung_A10', NULL, '2020-01-01 20:57:19'),
+(4, 'Laptop_i5', NULL, '2020-01-01 20:57:19'),
+(5, 'Laptop_i3', NULL, '2020-01-01 20:57:19'),
+(6, 'Laptop_i7', NULL, '2020-01-01 20:57:19');
+
 -- --------------------------------------------------------
 
 --
@@ -147,6 +218,17 @@ CREATE TABLE `devices_to_employees` (
   `employee_id` int(11) DEFAULT NULL,
   `device_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `devices_to_employees`
+--
+
+INSERT INTO `devices_to_employees` (`id`, `employee_id`, `device_id`) VALUES
+(1, 1, 5),
+(2, 1, 2),
+(3, 2, 6),
+(4, 2, 2),
+(5, 4, 2);
 
 -- --------------------------------------------------------
 
@@ -165,6 +247,16 @@ CREATE TABLE `employees` (
   `phone` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`id`, `full_name`, `created_at`, `office_id`, `department_id`, `position_id`, `email`, `phone`) VALUES
+(1, 'Ion Berindei', '2020-01-01 21:00:21', 0, 1, 1, 'IonBerindei@gmail.com', '0700'),
+(2, 'Mihai Tudorache', '2020-01-02 11:02:45', 0, 3, 2, 'tudorache@gmail.com', '81219'),
+(3, 'Geta Rozetar', '2020-01-02 11:03:46', 0, 4, 2, NULL, NULL),
+(4, 'Marcu Petrisan', '2020-01-02 11:04:03', 0, 3, 2, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -176,6 +268,15 @@ CREATE TABLE `employeespositions` (
   `name` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employeespositions`
+--
+
+INSERT INTO `employeespositions` (`id`, `name`, `description`) VALUES
+(1, 'inexperienced', NULL),
+(2, 'experienced', NULL),
+(3, '1-3 ani ', NULL);
 
 -- --------------------------------------------------------
 
@@ -192,6 +293,20 @@ CREATE TABLE `items` (
   `price` int(11) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `description`, `category_id`, `warehouse_id`, `price`, `status`) VALUES
+(1, 'TV SET 100 inch ', NULL, 1, 1, 100, 'New'),
+(2, '2.1 system 1000 W', NULL, 2, 1, 60, 'Used'),
+(3, 'Round Table', NULL, 3, 1, 10, 'Used'),
+(4, 'Warm light 100 W', NULL, 4, 1, 10, 'New'),
+(5, 'Samsung VR Set', NULL, 5, 1, 50, 'New'),
+(6, 'Display cu integrare vco', 'Se integreaza la camerele de trafic persoane', 1, 1, 400, 'Disponibil'),
+(7, 'Set luminite ptr fetite', 'Sunt dragutze si purpurii', 4, 2, 100, 'momentan le-am dat la un prieten de revelion'),
+(8, 'Set scaune ikea', 'Sunt alea galbene de statura mica. au in set si un taburet', 3, 1, 120, 'disponibil');
 
 -- --------------------------------------------------------
 
@@ -225,9 +340,17 @@ CREATE TABLE `orders` (
   `client_id` int(11) NOT NULL,
   `status` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `delivery_type` int(11) DEFAULT NULL,
-  `bill_id` int(11) DEFAULT NULL
+  `delivery_type` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `client_id`, `status`, `created_at`, `delivery_type`) VALUES
+(1, 2, 'se livreaza', '2020-01-02 10:55:39', 3),
+(2, 3, 'refuzat', '2020-01-02 11:00:01', 3),
+(3, 1, 'se livreaza', '2020-01-02 11:00:57', 2);
 
 -- --------------------------------------------------------
 
@@ -241,6 +364,16 @@ CREATE TABLE `order_items` (
   `booking_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`order_id`, `item_id`, `booking_id`, `quantity`) VALUES
+(1, 2, 1, 1),
+(1, 4, 1, 3),
+(3, 1, 2, 1),
+(3, 2, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -260,10 +393,10 @@ CREATE TABLE `payments` (
 --
 
 INSERT INTO `payments` (`id`, `value`, `payment_type_id`, `created_at`) VALUES
-(1, 120, 1, '2020-01-02 10:35:44'),
-(2, 4, 1, '2020-01-02 10:36:36'),
-(3, 240, 1, '2020-01-02 10:36:36'),
-(4, 1000, 2, '2020-01-02 10:37:20');
+(1, 10, 1, '2020-01-02 11:11:28'),
+(2, 23, 1, '2020-01-02 11:11:28'),
+(3, 150, 1, '2020-01-02 11:11:28'),
+(4, 80, 1, '2020-01-02 11:11:28');
 
 -- --------------------------------------------------------
 
@@ -276,6 +409,16 @@ CREATE TABLE `payments_to_bills` (
   `bill_id` int(11) DEFAULT NULL,
   `payment_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payments_to_bills`
+--
+
+INSERT INTO `payments_to_bills` (`id`, `bill_id`, `payment_id`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 3, 4);
 
 -- --------------------------------------------------------
 
@@ -348,7 +491,8 @@ INSERT INTO `warehouses` (`id`, `name`, `country_code`, `address`) VALUES
 -- Indexes for table `bills`
 --
 ALTER TABLE `bills`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`);
 
 --
 -- Indexes for table `bookings`
@@ -437,7 +581,7 @@ ALTER TABLE `offices`
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `delivery_type` (`delivery_type`),
-  ADD KEY `bill_id` (`bill_id`);
+  ADD KEY `client_id` (`client_id`);
 
 --
 -- Indexes for table `order_items`
@@ -489,73 +633,79 @@ ALTER TABLE `warehouses`
 -- AUTO_INCREMENT for table `bills`
 --
 ALTER TABLE `bills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `delivery_types`
 --
 ALTER TABLE `delivery_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `devices`
 --
 ALTER TABLE `devices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `devices_to_employees`
 --
 ALTER TABLE `devices_to_employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `employeespositions`
 --
 ALTER TABLE `employeespositions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `payments`
@@ -567,7 +717,7 @@ ALTER TABLE `payments`
 -- AUTO_INCREMENT for table `payments_to_bills`
 --
 ALTER TABLE `payments_to_bills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `payment_types`
@@ -590,6 +740,12 @@ ALTER TABLE `warehouses`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `bills`
+--
+ALTER TABLE `bills`
+  ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
 
 --
 -- Constraints for table `clients`
@@ -616,7 +772,7 @@ ALTER TABLE `employees`
 -- Constraints for table `items`
 --
 ALTER TABLE `items`
-  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `items` (`id`),
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
   ADD CONSTRAINT `items_ibfk_2` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses` (`id`);
 
 --
@@ -630,7 +786,7 @@ ALTER TABLE `offices`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`delivery_type`) REFERENCES `delivery_types` (`id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`id`);
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
 
 --
 -- Constraints for table `order_items`
